80d7829254adb1cc56c5a0eb3a108f3b4879f811
Bill Gutierrez <darckneck@hotmail.com>
2023-12-08 19:28:31 -0500

    Playlist despliegue5

--- datos/urls.py
+++ datos/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.datos, name='datos'),
     path('envia/', views.envia, name='envia'),
     path('envia_cancion/', views.envia_cancion, name='envia_cancion'),
+    path('envia_playlist/', views.envia_playlist, name='envia_playlist'),
+    path('traer_playlist/', views.traer_playlist, name='traer_playlist'),
 ]
--- datos/views.py
+++ datos/views.py
@@ -11,17 +11,53 @@ def datos(request):
     return render(request, template, {'data':data})
 
 def envia(request):
-    nuevo_artista = {'nombre':'cn59', 'genero':'alternaativo'}
+    nuevo_artista = {'nombre':'jenny', 'genero':'regge'}
     response = requests.post(ruta, json=nuevo_artista)
     data = response.json()
     return render(request, template, {'data':data})
 
 def envia_cancion(request):
     nueva_cancion = {
-        'titulo':'una cancion de perdon',
-        'artista':1,
-        'letras':'letras de la cancion'
+        'titulo':'cancion de amo',
+        'artista':3,
+        'letras':'hace mucho no sentia lo que siento en este dia'
     }
     response = requests.post('https://bill-despliegue-drf.onrender.com/cancion/', json=nueva_cancion)
     data = response.json
     return render(request, template, {'data':data})
+
+def envia_playlist(request):
+    nueva_playlist = {
+        'nombre': 'Mi Lista de Reproducción favorita',
+        'descripcion': 'Descripción de la lista de reproducción 2',
+        'canciones': [1, 2, 3]  # IDs de las canciones que pertenecen a la lista de reproducción
+    }
+
+    response = requests.post('https://bill-despliegue-drf.onrender.com/playlist/', json=nueva_playlist)
+    data = response.json()
+
+    return render(request, template, {'data': data})
+
+import requests
+
+def traer_playlist(request):
+    response = requests.get('https://bill-despliegue-drf.onrender.com/playlist/')
+    playlists = response.json()
+
+    # Obtener detalles de cada canción en cada lista de reproducción
+    for playlist in playlists:
+        playlist['canciones_detalles'] = []
+        for cancion_id in playlist['canciones']:
+            cancion_details = obtener_detalles_cancion(cancion_id)
+            playlist['canciones_detalles'].append(cancion_details)
+        
+        del playlist['canciones']
+    
+    nombre = playlists[1]['canciones_detalles'][2]['titulo']
+    
+    return render(request, template, {'playlists': playlists, 'nombre':nombre})
+
+def obtener_detalles_cancion(cancion_id):
+    response = requests.get(f'https://bill-despliegue-drf.onrender.com/cancion/{cancion_id}/')
+    cancion_details = response.json()
+    return cancion_details
